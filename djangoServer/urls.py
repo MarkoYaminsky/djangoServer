b10ab7f26595123e4b7687065d73445ee49d9f4a
Marko Yaminsky <marko.yaminskiy03@gmail.com>
2022-04-20 02:28:56 +0300

    added student viewset, introduced the slug system (questionable)

--- djangoServer/urls.py
+++ djangoServer/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 from studentsapp import views
+from rest_framework import routers
 
 
 app_name = 'students'
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('students/', views.StudentList.as_view()),
-    path('students/<pk>', views.StudentDetails.as_view())
-]
+router = routers.SimpleRouter()
+router.register(r'students', views.StudentViewSet)
+urlpatterns = router.urls
--- studentsapp/models.py
+++ studentsapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django_extensions.db.fields import SlugField
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -6,7 +8,12 @@ class Student(models.Model):
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
     age = models.IntegerField()
+    slug = SlugField(unique=True, null=True)
     objects = models.Manager()
 
     def __str__(self):
         return self.first_name
+
+    def save(self, *args, **kwargs):
+        self.slug = '_'.join([slugify(self.first_name), slugify(self.last_name), slugify(self.id)])
+        super(Student, self).save(*args, **kwargs)
